dfca7fff45d687babd88131f57bef2df3bd6ca70
Matt Gordon <bigmattsize21@gmail.com>
2020-05-04 00:52:19 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,8 +35,7 @@ def select_series_title_with_most_human_characters
         ON series.id = characters.series_id
       WHERE characters.species = 'human'
       GROUP BY characters.series_id
-      ORDER BY num_species DESC
-      LIMIT 1);"
+      ORDER BY num_species DESC);"
 end
 
 def select_character_names_and_number_of_books_they_are_in
